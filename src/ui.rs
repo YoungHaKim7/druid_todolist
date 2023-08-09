baa91a3ac2d1b5c8a8ed737acbe6fc6491fbde76
GYoung <ytok1108@aol.com>
2023-08-09 20:42:55 +0900

    UI Text Box___Flex

--- src/ui.rs
+++ src/ui.rs
@@ -7,7 +7,7 @@ use crate::data::TodoState;
 
 pub fn ui_builder() -> impl Widget<TodoState> {
     let header = Flex::row()
-        .with_child(TextBox::new().lens(TodoState::new_text))
+        .with_flex_child(TextBox::new().lens(TodoState::new_text).expand_width(), 1.)
         .with_child(Button::new("->"));
 
     Flex::column().with_child(header)
